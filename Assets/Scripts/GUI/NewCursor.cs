3d9bed6ae50bd4bac2fe02ebb446f8c5ce50bf14
lavil <vilhunen.laura@outlook.com>
2016-02-05 15:34:31 +0200

    Update #3
    Cubeja ja cursorikokeiluja

--- Assets/Scripts/GUI/NewCursor.cs
+++ Assets/Scripts/GUI/NewCursor.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class NewCursor : MonoBehaviour {
 
-	public Texture2D cursor;
+	public Texture2D attack;
+	public Texture2D select;
 	
 	// Update is called once per frame
 	void OnMouseEnter () {
-		Cursor.SetCursor (cursor, Vector2.zero, CursorMode.Auto);
+		if (gameObject.tag == "Player2") {
+			Cursor.SetCursor (attack, Vector2.zero, CursorMode.Auto);
+		}
+		else if (gameObject.tag == "Player1") {
+			Cursor.SetCursor (select, Vector2.zero, CursorMode.Auto);
+		}
 	}
 
 	void OnMouseExit() {
