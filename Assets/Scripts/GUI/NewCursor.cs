44112d10d7cfe1d74eb61044fecf8a3185e6a2d6
lavil <vilhunen.laura@outlook.com>
2016-02-05 10:08:13 +0200

    New Cursor Update

---
+++ Assets/Scripts/GUI/NewCursor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewCursor : MonoBehaviour {
+
+	public Texture2D cursor;
+	
+	// Update is called once per frame
+	void OnMouseEnter () {
+		Cursor.SetCursor (cursor, Vector2.zero, CursorMode.Auto);
+	}
+
+	void OnMouseExit() {
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+	}
+}
