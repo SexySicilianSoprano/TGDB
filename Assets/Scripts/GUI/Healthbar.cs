cc707a1e666edd227bb63754969b70a919e0ed59
lavil <n3vila00@students.oamk.fi>
2016-02-12 16:21:57 +0200

    Done for today

--- Assets/Scripts/GUI/Healthbar.cs
+++ Assets/Scripts/GUI/Healthbar.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class Healthbar : MonoBehaviour {
+[RequireComponent(typeof(RTSEntity))]
+public class HealthBarArmi : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public RectTransform canvasRectT;
+    public RectTransform healthBar;
+    public Image healthBarSlider;
+    public Transform objectToFollow;
+    public GameObject ImagePrefab;
+    public float currentHealth;
+    public float maxHealth;
+
+    void Start()
+    {
+        currentHealth = GetComponent<RTSEntity>().m_Health;
+        maxHealth = GetComponent<RTSEntity>().m_MaxHealth;
+        GameObject newHealthSlider = Instantiate(ImagePrefab, this.gameObject.transform.position, Quaternion.identity) as GameObject;
+        newHealthSlider.transform.SetParent(canvasRectT, true);
+        newHealthSlider.transform.position = new Vector3(0, 0, 0);
+        healthBar = (RectTransform)newHealthSlider.transform;
+        healthBarSlider = newHealthSlider.gameObject.GetComponent<Image>();
+        objectToFollow = this.gameObject.transform;
+    }
+
+    void Update()
+    {
+        Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, objectToFollow.position);
+        healthBar.anchoredPosition = screenPoint - canvasRectT.sizeDelta / 2f;
+        healthBarSlider.fillAmount = GetComponent<RTSEntity>().m_Health;
+        healthBarSlider.maxValue = GetComponent<RTSEntity>().m_MaxHealth;
+
+        if (gameObject.layer == 9)
+        {
+            healthBarSlider.gameObject.SetActive(false);
+            if (GetComponent<RTSEntity>().m_Health < GetComponent<RTSEntity>().m_MaxHealth)
+            {
+                healthBarSlider.gameObject.SetActive(true);
+            }
+        }
+    }
+
 }
