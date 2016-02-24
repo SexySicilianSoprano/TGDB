a5afe691f6ef7b81202394a413c231b873ea1caa
lavil <n3vila00@students.oamk.fi>
2016-02-24 11:03:29 +0200

    toimiikohan

--- Assets/Currents/Scripts/Gravity/CurrentGravity.cs
+++ Assets/Currents/Scripts/Gravity/CurrentGravity.cs
@@ -1,26 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CurrentGravity : MonoBehaviour {
-    
-    public float yAxis = 0;
-    public float xAxis = 0;
-    public float zAxis = 0;    
-    public float velocity = 0; // the speed
+/// <summary>
+/// This is the <CurrentGravity> script. Name's kinda stupid, I know.
+/// This component is attached to objects that become pieces of a current.
+/// Adjust velocity from Unity editor, note that the velocity is divided by 10 to give more
+/// room for fine-tuning if need be.So probably you should add more than 100 to actually move something.
+///
+/// Of course, feel free to change this anytime.
+///
+/// As you may notice, the direction where force is added comes from the component's host
+/// gameobject's transform.forward, which means you only need to point the object's blue arrow in editor
+/// towards the direction you want force be added to.
+///
+/// - Karl Sartorisio
+///
+/// The Great Deep Blue
+/// </summary>
 
-	void OnTriggerStay (Collider other)
+public class CurrentGravity : MonoBehaviour {
+
+    // Adjust velocity to add more force into colliding objects
+    public float velocity = 0;
+
+    // Adjust divider if you want to have bigger numbers to work with, in case you want smaller or larger room for adjustments
+    private float divider = 100f;
+
+    // Collides with things, make sure this gameobject's collider is a trigger, otherwise it won't work
+   	void OnTriggerStay (Collider other)
     {
-        // Move colliding rigidbodies to a direction in set velocity
-        
+        // Is the colliding object a building?      
         if (other.gameObject.GetComponent<Building>())
-        {
-            other.GetComponent<Rigidbody>().AddForce(xAxis * (velocity / 100), yAxis * (velocity / 100), zAxis * (velocity / 100));
+        {            
+            // Just move with the current, baby! Add force into colliding object
+            other.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.forward * (velocity / divider));
             Debug.Log("testi");
-        }
+        } 
+        // Is the colliding object an unit that's affected by currents?
         else if (other.gameObject.GetComponent<Unit>() && other.gameObject.GetComponent<VehicleMovement>().AffectedByCurrent)
-        {
-            other.GetComponent<Rigidbody>().AddForce(xAxis * (velocity / 100), yAxis * (velocity / 100), zAxis * (velocity / 100));
+        {            
+            // Just move with the current, baby! Add force into colliding object
+            other.GetComponent<Rigidbody>().AddForce(this.gameObject.transform.forward * (velocity / divider));
         }       
     }
-
 }
