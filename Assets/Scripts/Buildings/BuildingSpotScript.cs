709676ad58c420d1964684caee418471f9a96522
SexualTension <n4saka00@students.oamk.fi>
2016-03-03 16:18:05 +0200

    ~

---
+++ Assets/Scripts/Buildings/BuildingSpotScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingSpotScript : MonoBehaviour {
+
+    public bool isOccupied = false;
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.GetComponent<RTSEntity>())
+        {
+            isOccupied = true;
+        }
+    }
+}
