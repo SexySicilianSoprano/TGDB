429e30ea255c5772829440e51779b7192093ae86
lavil <n3vila00@students.oamk.fi>
2016-03-10 14:16:21 +0200

    healthbars logic implemented I guess??????

--- Assets/Scripts/GUI/HPBar.cs
+++ Assets/Scripts/GUI/HPBar.cs
@@ -6,28 +6,47 @@ using UnityEngine.UI;
 /// HPBar. This code determines how the HP Bar above game objects behave. Still need some logic work, although the basics are there!
 /// </summary>
 
+[RequireComponent(typeof(RTSEntity))]
 public class HPBar : MonoBehaviour {
 
     public RectTransform canvasRectT;
     public RectTransform healthBar;
     public Transform objectToFollow;
-    public Image Bar;
-    public float max_health = 100f;
-    public float cur_health = 0f;
+    public GameObject imgPrefab;
+    public Image bar;
+    public float max_health;
+    public float cur_health;
 
 	// Use this for initialization
 	void Start () {
-        cur_health = max_health;
-        InvokeRepeating ("decreaseHealth", 0f, 2f);
+        cur_health = GetComponent<RTSEntity>().m_Health;
+        max_health = GetComponent<RTSEntity>().m_MaxHealth;
+        GameObject newHealthBar = Instantiate(imgPrefab, this.gameObject.transform.position, Quaternion.identity) as GameObject;
+        newHealthBar.transform.SetParent(canvasRectT);
+        newHealthBar.transform.position = new Vector3 (0, 0, 0);
+        healthBar = (RectTransform)newHealthBar.transform;
+        bar = newHealthBar.gameObject.GetComponent<Image>();
+        objectToFollow = this.gameObject.transform;
+        //InvokeRepeating ("decreaseHealth", 0f, 2f);
 	}
 	
     void Update()
     {
         Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, objectToFollow.position);
         healthBar.anchoredPosition = screenPoint - canvasRectT.sizeDelta / 2f;
+        bar.fillAmount = GetComponent<RTSEntity>().GetHealthRatio();
+        
+        if (gameObject.layer == 9) 
+        {
+            bar.gameObject.SetActive(false);
+            if (GetComponent<RTSEntity>().m_Health < GetComponent<RTSEntity>().m_MaxHealth)
+            {
+                bar.gameObject.SetActive(true);
+            }
+        }
     }
 
-	void decreaseHealth () {
+	/*void decreaseHealth () {
         cur_health -= 5f;
         float calc_health = cur_health / max_health;
         SetHealth(calc_health);
@@ -35,6 +54,6 @@ public class HPBar : MonoBehaviour {
 
     void SetHealth (float myHealth)
     {
-        Bar.fillAmount = myHealth;
-    }
+        bar.fillAmount = myHealth;
+    }*/
 }
