482e54f5881707ba153212ebf9fafb1c0cb52b97
lavil <n3vila00@students.oamk.fi>
2016-06-07 14:11:34 +0300

    just editing stuff

--- Assets/MainMenu/Scripts/QuitApplication.cs
+++ Assets/MainMenu/Scripts/QuitApplication.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class QuitApplication : MonoBehaviour {
 
+    /// <summary>
+    /// This Script is made for quitting the game. Put this for UI buttons as OnClick() command for it to work, from MenuUI/PauseUI etc. game objects.
+    /// </summary>
+
 	public void Quit()
 	{
 		//If we are running in a standalone build of the game
